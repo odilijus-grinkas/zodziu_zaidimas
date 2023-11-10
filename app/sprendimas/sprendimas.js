b4bff7218a30032b48625d023f1f5d8133307fc8
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2023-11-10 21:49:42 +0200

    horizontal direction added

--- app/sprendimas/sprendimas.js
+++ app/sprendimas/sprendimas.js
@@ -8,20 +8,33 @@ function sprendimas(x, y, direction, word) {
   let nodes = [];
   if (direction == 1) {
     // horizontal
-    for (let i = 0; i <= word.length; i++) {}
+    for (let i = 1; i <= word.length; i++) {
+      if (!outOfBounds(i, y)) {
+        nodes.push(letterFinder(i,y));
+      } else continue;
+    }
   }
 }
 
 function letterFinder(x, y) {
+  // What formatting is used before/after the number?
+  formatX = x + "row";
+  formatY = y + "col";
   for (let node of windows) {
-    if (node.classList.contains("" + x) && node.classList.contains("" + y)) {
+    if (node.classList.contains(formatX) && node.classList.contains(formatY)) {
       return node;
     }
   }
-  throw new Error("Coordinate does not exist in provided array");
+  throw new Error(
+    "Coordinate does not exist in provided array OR x y formatting is incorrect"
+  );
 }
 
 // This assumes table is a square
-function outOfBoundsChecker(x, y, tableSize) {
-  
+function outOfBounds(x, y, tableSize = 10) {
+  if (x <= tableSize && y <= tableSize && x > 0 && y > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
