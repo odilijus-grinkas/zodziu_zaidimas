b98c5affd54dbece219426ea67ebc28dac922daf
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2023-11-10 12:59:31 +0200

    changed more words

--- app/add_letters.js
+++ app/add_letters.js
@@ -1,6 +1,6 @@
-function addLetters(letterArray, wordTable) {
+function addLetters(letterArray, letterTable) {
     let i = 0;
-    for (let e of wordTable) {
+    for (let e of letterTable) {
         e.innerHTML = letterArray[i];
         i++;
     }
