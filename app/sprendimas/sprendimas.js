d1e5d2d5f26500e2518dfdd45ea200ba816f7a73
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2023-11-10 21:29:49 +0200

    added letterFunder and some spredimas

---
+++ app/sprendimas/sprendimas.js
@@ -0,0 +1,25 @@
+// imports
+
+// list of window elements..
+const windows = document.getElementsByClassName("window");
+
+function sprendimas(x, y, direction, word) {
+    const firstLetter = letterFinder(x, y);
+    let nodes = [];
+    if (direction == 1) {
+        // horizontal
+        for (let i = 0; i <= word.length; i++) {}
+    }
+}
+
+function letterFinder(x, y) {
+    for (let node of windows) {
+        if (
+            node.classList.contains("" + x) &&
+            node.classList.contains("" + y)
+        ) {
+            return node;
+        }
+    }
+    throw new Error("Coordinate does not exist in provided array");
+}
