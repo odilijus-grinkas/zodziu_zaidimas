7e20f0e289bd8fc1b9f3c27067ab16ac4f61430d
DominykasKupsys <dominykas.kupsys@stud.ktmc.lt>
2023-11-10 20:23:24 +0200

    added style and  generated_words function

--- app/start.js
+++ app/start.js
@@ -4,10 +4,12 @@ import{table,test} from "./board_generator.js"
 import { addLetters } from "./add_letters.js"
 import { connecting } from "./zaidimai.js"
 import { addWords } from "./word_func.js";
+import { zodziuLentele } from "./generated_words.js";
 // document.getElementById("test").innerHTML = table;
 // let array = connecting(1);
 test();
 let letters = connecting(1)[0];
 let words = connecting(1)[1]
 addLetters(letters,document.getElementsByClassName("window"))
+zodziuLentele(5);
 addWords(document.getElementsByClassName("given_word"),words)
