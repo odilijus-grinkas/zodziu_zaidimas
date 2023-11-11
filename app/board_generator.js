310fb99951656d91d5037bc43984a6c5b498fdc7
TomasNopenas <tomas.nopenas@stud.ktmc.lt>
2023-11-11 14:43:20 +0200

    added coords to generator

--- app/board_generator.js
+++ app/board_generator.js
@@ -1,18 +1,20 @@
-let table = "<table>";
-    for(let i = 1; i <= 10; i++){
-        if(i % 10 === 0){
-            table += "<tr>";
-        }
-    for(let j = 1; j <= 10; j++){
-        table += "<td class=\"window\"></td>";
-        if(j % 10 === 0){
-            table += "</tr>";
-        }
+function board_generation(){
+    let table = "<table>";
+    const row = ["x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10"];
+    const col = ["y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9", "y10"];
+for(let i = 1; i <= 10; i++){
+    if(i % 10 === 0){
+        table += "<tr>";
     }
-    } // T
-    table += "</table>"
-    document.getElementById("lentele").innerHTML = table;
-function test(){
-    document.getElementById("lentele").innerHTML = table; 
+for(let j = 1; j <= 10; j++){
+    table += "<td class=\"window" + " " + row[j-1] + " " + col[i-1] + "\">" + "</td>";
+    if(j % 10 === 0){
+        table += "</tr>";
+    }
 }
+}
+table += "</table>";
+return table;
+}
+document.getElementById("lentele").innerHTML = board_generation();
 export{table,test}
