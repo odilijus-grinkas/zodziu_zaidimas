86476b7b0e6b04f871956acdfca9a77ca0bd3999
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2023-11-11 15:24:10 +0200

    comments

--- app/start.js
+++ app/start.js
@@ -5,11 +5,12 @@ import { addLetters } from "./add_letters.js"
 import { connecting } from "./zaidimai.js"
 import { addWords } from "./word_func.js";
 import { board_generation } from "./board_generator.js";
-// document.getElementById("test").innerHTML = table;
-// let array = connecting(1);
+// Make table
 document.getElementById("lentele").innerHTML = board_generation();
+// Add picked letters and words to table
 let letters = connecting(1)[0];
 let words = connecting(1)[1]
 addLetters(letters,document.getElementsByClassName("window"))
 addWords(document.getElementsByClassName("given_word"),words)
+// Play Game
 sprendimas(3,3,3,"Happy");
