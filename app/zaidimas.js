ce9b2c74b1effc3289eb9810afaa10b7bc96d48e
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2023-11-13 19:39:31 +0200

    merger

--- app/zaidimas.js
+++ app/zaidimas.js
@@ -1,18 +1,19 @@
 // Combine modules here
 // import{connecting} from "./zaidimai"
 import { sprendimas } from "./sprendimas/sprendimas.js";
-import { addLetters } from "./add_letters.js"
-import { connecting } from "./zaidimai.js"
+import { addLetters } from "./add_letters.js";
 import { addWords } from "./word_func.js";
 import { board_generation } from "./board_generator.js";
+import { zodziuLentele } from "./generated_words.js";
+import { firstGame } from "./zaidimai/zaidimas1.js";
+import { secondGame } from "./zaidimai/zaidimas2.js";
 // Make table
 document.getElementById("lentele").innerHTML = board_generation();
-// Add picked letters and words to table
-import { zodziuLentele } from "./generated_words.js";
-let letters = connecting(1)[0];
-let words = connecting(1)[1]
-addLetters(letters,document.getElementsByClassName("window"))
 zodziuLentele(5);
+// Add picked letters and words to table
+let letters = firstGame()[0];
+let words = firstGame()[1]
+addLetters(letters,document.getElementsByClassName("window"))
 addWords(document.getElementsByClassName("given_word"),words)
 // Play Game
-sprendimas(3,3,3,"Happy");
+sprendimas(3,3,2,"Happy");
