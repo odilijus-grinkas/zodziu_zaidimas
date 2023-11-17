03b27d86b831f539c0312fe6678af4644d4ee2d8
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2023-11-17 09:51:54 +0200

    HI

--- app/zaidimai/zaidimas1.js
+++ app/zaidimai/zaidimas1.js
@@ -1,16 +1,16 @@
 // Word Search Game 1
 // Random letters 10x10 + Hidden Words
 const randomLetters = [
-    "C", "M", "F", "W", "J", "U", "U", "L", "N", "R",
-    "B", "W", "L", "O", "S", "T", "Y", "L", "V", "J",
-    "T", "X", "A", "P", "H", "X", "P", "Q", "D", "B",
-    "D", "W", "M", "T", "E", "L", "P", "N", "T", "H",
-    "B", "U", "T", "R", "E", "E", "A", "T", "H", "Z",
-    "J", "Q", "L", "K", "P", "R", "H", "A", "F", "W",
-    "F", "A", "E", "R", "H", "P", "T", "Z", "N", "Q",
-    "U", "F", "C", "L", "H", "N", "L", "N", "O", "T",
-    "F", "D", "O", "N", "H", "I", "W", "V", "I", "S",
-    "V", "V", "X", "G", "N", "V", "K", "G", "T", "D"
+    "J", "V", "K", "N", "E", "T", "G", "O", "A", "B",
+    "Q", "Q", "H", "E", "E", "R", "U", "I", "C", "F",
+    "M", "U", "W", "A", "T", "E", "R", "L", "X", "C",
+    "J", "H", "N", "D", "S", "E", "M", "A", "I", "E",
+    "U", "D", "Z", "A", "C", "H", "A", "P", "P", "Y",
+    "M", "X", "K", "T", "S", "P", "W", "Z", "C", "A",
+    "P", "H", "Z", "J", "Q", "B", "B", "P", "B", "U",
+    "Q", "O", "F", "V", "L", "R", "M", "P", "K", "O",
+    "Q", "H", "H", "S", "H", "E", "E", "P", "V", "N",
+    "J", "M", "L", "H", "J", "B", "O", "J", "C", "N",
 ];
 
 // Word List
@@ -22,11 +22,49 @@ const wordList = [
     "SHEEP"
 ];
 
-function firstGame() {
-    const option = [
-        randomLetters,
-        wordList
+
+// Word Search Game 2
+// Random letters 10x10 + Hidden Words
+const randomLettersTwo = [
+    "G", "P", "E", "K", "L", "J", "N", "G", "G", "K",
+    "R", "S", "A", "E", "Z", "I", "R", "I", "W", "O",
+    "W", "D", "P", "N", "U", "N", "R", "R", "N", "Z",
+    "U", "X", "N", "G", "D", "P", "O", "A", "P", "W",
+    "H", "U", "N", "H", "S", "A", "W", "F", "T", "S",
+    "E", "E", "Y", "A", "I", "N", "Y", "F", "F", "G",
+    "P", "F", "K", "B", "U", "L", "C", "E", "C", "Q",
+    "G", "O", "A", "T", "I", "K", "D", "G", "W", "A",
+    "B", "P", "J", "O", "I", "K", "O", "E", "E", "Z",
+    "Q", "Q", "N", "B", "X", "I", "A", "R", "T", "N"
+];
+
+// Word List
+const animalsWordList = [
+    "Penguin",
+    "Goat",
+    "Panda",
+    "Giraffe",
+    "Lion"
+];
+
+
+// buttons - dviems zaidimams (connecting) 
+// viena array su funckiju names 
+// if else if + skaiciai 1-game one, 2-game two
+
+function connecting(number) {
+    const first = [
+        randomLetters,wordList
     ];
-    return option;
+    const second = [
+        randomLettersTwo,
+        animalsWordList
+    ];
+
+    if (number == 1) {
+        return first
+    } else if (number == 2) {
+        return second
+    }
 };
-export{firstGame};
+export{connecting}
